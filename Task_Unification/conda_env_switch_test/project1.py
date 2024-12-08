5e4177f9b39175d889ef49b66ade52681cf500e5
Aryan Singh <b22260@students.iitmandi.ac.in>
2024-12-08 08:52:16 +0100

    added project1.py

---
+++ Task_Unification/conda_env_switch_test/project1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+def run_project1():
+    print("Project 1 Dependencies:")
+    print(np.__version__)
+    print(pd.__version__)
+
+if __name__ == "__main__":
+    run_project1()
