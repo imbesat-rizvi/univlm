5361300c41bc9e9c689286b5f235c0a4baefddb4
Aryan Singh <b22260@students.iitmandi.ac.in>
2024-12-08 08:53:36 +0100

    added project2.py

---
+++ Task_Unification/conda_env_switch_test/project2.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+#from sklearn.model_selection import train_test_split
+
+def run_project2():
+    print("Project 2 Dependencies:")
+    print(np.__version__)
+    print(pd.__version__)
+    print("Aryan Is Great!")
+
+if __name__ == "__main__":
+    run_project2()
